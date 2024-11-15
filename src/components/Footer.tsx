8b02ca0cdd840745a798b5c27e3fb41fdfcdc18f
Rabiawaheed786 <162490318+Rabiawaheed786@users.noreply.github.com>
2024-11-15 08:55:50 +0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <>
-      <footer className="bg-[#0a0908] text-[#a9927d] py-6 text-center text-md lg:text-2xl font-[Caveat]">© 2024 Rabia bi bi. All rights reserved.</footer>
+      <footer className="bg-[#0a0908] text-[#a9927d] py-6 text-center text-md lg:text-2xl font-[Caveat]">© 2024 Rabia Waheed. All rights reserved.</footer>
     </>
   )
 }
